b16cd83e02ffc505f1653385873148c77b35af6d
Aditya Maharana <aditya11maharana@gmail.com>
2021-12-12 20:44:02 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,27 +1,74 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
-import './App.css';
+import styles from './App.module.css';
 import Dropdown from './features/Dropdown/Dropdown';
 
 function App() {
 	const [data, setData] = useState([]);
 
+	const [areSelected, setAreSelected] = useState([]);
+
+	const [selectedStates, setSelectedStates] = useState([]);
+
 	useEffect(() => {
 		axios.get('http://localhost:5000/data').then((res) => {
 			setData(res.data);
-			console.log(res.data);
 		});
 	}, []);
+	// useEffect(() => {
+	// 	selectedStates;
+	// });
+
+	const onChangeHandler = (item, type) => {
+		if (!selectedStates.some((arr) => arr.id === item.id)) {
+			setSelectedStates([...selectedStates, item]);
+		} else {
+			var temp = selectedStates;
+			temp = temp.filter((e) => e.id !== item.id);
+			setSelectedStates([...temp]);
+		}
+		temp = [];
+		selectedStates.forEach((e) => temp.push(e.state));
+	};
+	const handleOnChange = (item) => {
+		if (!areSelected.some((arr) => arr.id === item.id)) {
+			setAreSelected([...areSelected, item]);
+		} else {
+			var temp = areSelected;
+			temp = temp.filter((e) => e.id !== item.id);
+			setAreSelected([...temp]);
+		}
+	};
 
 	return (
-		<div>
+		<div className={styles.container}>
 			{data.map((data) => {
 				return (
 					<div>
 						{<div>{data.country}</div>}
 						{data.state.map((data) => {
-							return <Dropdown title={data.state} items={data.city}></Dropdown>;
+							return (
+								<div className={styles.state}>
+									<div className={styles.checkBox}>
+										<input
+											type='checkbox'
+											onChange={() => onChangeHandler(data, 'state')}>
+											{/* {selectedStates.find((e) => e.id === data.id)
+											? 'Selected'
+											: 'Not Selected'} */}
+										</input>
+									</div>
+
+									<div className={styles.dropdown}>
+										<Dropdown
+											title={data.state}
+											items={data.city}
+											areSelected={areSelected}
+											handleOnChange={handleOnChange}></Dropdown>
+									</div>
+								</div>
+							);
 						})}
 					</div>
 				);
--- src/features/Dropdown/Dropdown.js
+++ src/features/Dropdown/Dropdown.js
@@ -1,19 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import styles from './Dropdown.module.css';
 
-export default function Dropdown({ title, items }) {
+export default function Dropdown({
+	title,
+	items,
+	areSelected,
+	handleOnChange,
+}) {
 	const [isOpen, setIsOpen] = useState(false);
-	const [areSelected, setAreSelected] = useState([]);
-	const handleOnClick = (item) => {
-		if (!areSelected.some((arr) => arr.id === item.id)) {
-			setAreSelected([...areSelected, item]);
-		} else {
-			var temp = areSelected;
-			temp = temp.filter((e) => e.id !== item.id);
-			setAreSelected([...temp]);
-		}
-	};
+
+	// useEffect(() => {
+	// 	if (allSelected) {
+	// 		setAreSelected(items);
+	// 		setIsOpen(true);
+	// 	} else {
+	// 		setAreSelected([]);
+	// 	}
+	// }, [allSelected, items, setAreSelected]);
 
 	return (
 		<div>
@@ -23,21 +27,20 @@ export default function Dropdown({ title, items }) {
 					setIsOpen(!isOpen);
 				}}>
 				<div className={styles.title}>{title}</div>
-				<div className={styles.toggle}>{isOpen ? 'Close' : 'Open'}</div>
 			</div>
 			{isOpen && (
 				<div>
 					{items.map((item) => (
-						<div key={item.id}>
-							<div>{item.name}</div>
-							<button
-								onClick={() => {
-									handleOnClick(item);
-								}}>
-								{areSelected.find((e) => e.id === item.id)
-									? 'Selected'
-									: 'Not Selected'}
-							</button>
+						<div key={item.id} style={{ marginTop: '5px' }}>
+							<input
+								type='checkbox'
+								checked={
+									areSelected.find((arr) => arr.id === item.id) ? true : false
+								}
+								onChange={() => {
+									handleOnChange(item);
+								}}></input>
+							<span className={styles.subItems}>{item.name}</span>
 						</div>
 					))}
 				</div>
